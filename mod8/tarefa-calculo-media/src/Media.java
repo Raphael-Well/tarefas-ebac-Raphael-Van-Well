ac503d788c42891933dc8d71306a6724e5bd5dff
Raphael Van Well <rapha.well@outlook.com>
2023-04-21 11:23:45 -0300

    Tarefa modulo 8

---
+++ mod8/tarefa-calculo-media/src/Media.java
@@ -0,0 +1,12 @@
+public class Media {
+    public static void main(String[] args) {
+        float nota1 = 8.5f;
+        float nota2 = 7.3f;
+        float nota3 = 9.2f;
+        float nota4 = 6.7f;
+
+        float media = (nota1+nota2+nota3+nota4)/4;
+
+        System.out.println(media);
+    }
+}
