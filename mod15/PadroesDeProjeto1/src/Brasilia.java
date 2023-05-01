ca22a1ec2d974413b4c206eead08b87d5322822b
Raphael Van Well <rapha.well@outlook.com>
2023-05-01 12:18:46 -0300

    Tarefa modulo 15

---
+++ mod15/PadroesDeProjeto1/src/Brasilia.java
@@ -0,0 +1,5 @@
+public class Brasilia extends Car{
+    public Brasilia(int horsePower, String fuelSource, String color) {
+        super(horsePower, fuelSource, color);
+    }
+}
---
+++ mod15/PadroesDeProjeto1/src/ContratosFactory.java
@@ -0,0 +1,10 @@
+public class ContratosFactory extends Factory {
+    @Override
+    Car retrieveCar(String requestedGrade) {
+        if ("A".equals(requestedGrade)) {
+            return new CorolaCar(100, "cheio", "azul");
+        } else {
+            return null;
+        }
+    }
+}
